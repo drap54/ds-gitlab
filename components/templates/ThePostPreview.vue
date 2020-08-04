95eb6f91c9a4e5716b3b4fa04cdf25164035e9c6
Dan Weather <spamdrap@gmail.com>
2020-08-04 19:16:39 +0800

    Inistial setup

---
+++ components/templates/ThePostPreview.vue
@@ -0,0 +1,73 @@
+<template>
+  <nuxt-link :to="'/marketing/' + path + '/' + slug" class="post-preview">
+    <article>
+      <div class="post-content">
+        <h3>{{ title }}</h3>
+        <p>{{ description }}</p>
+      </div>
+    </article>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'PostPreview',
+  props: {
+    path: {
+      type: String,
+      required: true
+    },
+    slug: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      default: '',
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: dodgerblue;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: cornflowerblue;
+}
+</style>
---
+++ components/templates/vjlp3/TheGameSlider.vue
@@ -0,0 +1,84 @@
+<template>
+  <section id="bf-game-slider">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <VueSlickCarousel
+            :arrows="false"
+            :dots="false"
+            :infinite="true"
+            :autoplay="true"
+            :speed="300"
+            :slidesToShow="5"
+            :slidesToScroll="1"
+            v-bind="carouselSettings"
+          >
+            <a class="hvr-shrink" v-for="(link, index) in links" :key="index" v-bind:href="link">
+              <div v-bind:style="generateSliderIcon(icons[index])"></div>
+            </a>
+          </VueSlickCarousel>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import VueSlickCarousel from "vue-slick-carousel";
+
+export default {
+  name: "GameSlider",
+  props: {
+    game: {
+      type: Object,
+      required: true
+    }
+  },
+  beforeMount() {
+    this.icons = [];
+    this.links = [];
+
+    Object.entries(this.game).map(([key, value]) => {
+      key.indexOf("game_icon_") === 0
+        ? value != ""
+          ? this.icons.push(value)
+          : null
+        : null;
+      key.indexOf("game_url_") === 0
+        ? value != ""
+          ? this.links.push(value)
+          : null
+        : null;
+    });
+  },
+  components: {
+    VueSlickCarousel
+  },
+  data() {
+    return {
+      links: [""],
+      icons: [""],
+      carouselSettings: {
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              autoplay: true
+            }
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    generateSliderIcon(icon) {
+      return {
+        background: `center / contain no-repeat url("${icon}")`
+      };
+    }
+  }
+};
+</script>
