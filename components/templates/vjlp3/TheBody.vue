eb10b109f099d08e7ab0684d5592fc0734261d17
Dan Weather <spamdrap@gmail.com>
2020-08-05 16:14:53 +0800

    Add files from kenkoy

---
+++ components/templates/vjlp3/TheBody.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-html="promo_content" />
+</template> 
+
+<script>
+export default {
+  name: 'Content',
+  props: {
+    promo_content: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+#bf-game-slider .slick-slider {
+  margin: 10px 0;
+}
+#bf-usps .row {
+  margin: 0;
+}
+</style>
