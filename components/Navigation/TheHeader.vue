be60ce88fb81b390f2dec498560500f08f8ecad5
Dan Weather <spamdrap@gmail.com>
2020-08-19 15:06:27 +0800

    Insert footer styling

--- components/Navigation/TheHeader.vue
+++ components/Navigation/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="header-container">
   <header class="the-header">
-    <TheSideNavToggle @toggle="$emit('sidenavToggle')" />
+    <TheSideNavToggle @toggle="$emit('sidenav-toggle')" />
     <div class="logo">
       <nuxt-link to="/marketing">
         <img src="~/assets/images/gamesys-logo.png"/>
