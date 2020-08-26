3ab900665938f6835ecd55a9454ccc639b7bc49e
Dan Weather <spamdrap@gmail.com>
2020-08-26 22:32:07 +0800

    Add header fields

--- .nuxt/gtm.js
+++ .nuxt/gtm.js
@@ -1,10 +1,64 @@
-// This is a mock version because gtm module is disabled
-// You can explicitly enable module using `gtm.enabled: true` in nuxt.config
-
 const _layer = 'dataLayer'
 const _id = 'GTM-MFD3NKM'
 
-function startPageTracking (ctx) {
+function gtmClient(ctx, initialized) {
+  return {
+    init(id = _id) {
+      if (initialized[id] || !window._gtm_inject) {
+        return
+      }
+      window._gtm_inject(id)
+      initialized[id] = true
+    },
+    push(obj) {
+      if (!window[_layer]) {
+        window[_layer] = [{ 'gtm.start': new Date().getTime(), event: 'gtm.js' }]
+      }
+      window[_layer].push(obj)
+    }
+  }
+}
+
+function gtmServer(ctx, initialized) {
+  const events = []
+  const inits = []
+
+  ctx.beforeNuxtRender(() => {
+    if (!inits.length && !events.length) {
+      return
+    }
+
+    const gtmScript = ctx.app.head.script.find(s => s.hid = 'gtm-script')
+    if (inits.length) {
+      gtmScript.innerHTML += `;${JSON.stringify(inits)}.forEach(function(i){window._gtm_inject(i)})`
+    }
+
+    const gtmIframe = ctx.app.head.noscript.find(s => s.hid = 'gtm-noscript')
+    const renderIframe = id => `<iframe src="https://www.googletagmanager.com/ns.html?id=${id}&" height="0" width="0" style="display:none;visibility:hidden" title="gtm"></iframe>`
+    if (inits.length) {
+      gtmIframe.innerHTML += inits.map(renderIframe)
+    }
+
+    if (events.length) {
+      gtmScript.innerHTML += `;${JSON.stringify(events)}.forEach(function(e){window['${_layer}'].push(e)})`
+    }
+  })
+
+  return {
+    init(id = _id) {
+      if (initialized[id]) {
+        return
+      }
+      inits.push(id)
+      initialized[id] = true
+    },
+    push(obj) {
+      events.push(obj)
+    }
+  }
+}
+
+function startPageTracking(ctx) {
   ctx.app.router.afterEach((to) => {
     setTimeout(() => {
       ctx.$gtm.push(to.gtm || {
@@ -19,20 +73,8 @@ function startPageTracking (ctx) {
 }
 
 export default function (ctx, inject) {
-  // eslint-disable-next-line no-console
-  const logSyle = 'background: #2E495E;border-radius: 0.5em;color: white;font-weight: bold;padding: 2px 0.5em;'
-  const log = (...args) => console.log('%cGTM', logSyle, ...args)
-
-  const gtm = {
-    init: (id) => {
-      log('init', id)
-    },
-    push: (event) => {
-      log('push', process.client ? event : JSON.stringify(event))
-    }
-  }
-
-  ctx.$gtm = gtm
-  inject('gtm', gtm)
+  const initialized = { 'GTM-MFD3NKM': true }
+  ctx.$gtm = process.client ? gtmClient(ctx, initialized) : gtmServer(ctx, initialized)
+  inject('gtm', ctx.$gtm)
   if (process.client) { startPageTracking(ctx); }
 }
--- components/templates/vjhp/TheHeaderSection.vue
+++ components/templates/vjhp/TheHeaderSection.vue
@@ -61,26 +61,94 @@
 export default {
   name: 'TheHeaderSection',
   props: {
-    header_text: {
+    // header_text: {
+    //   type: String,
+    //   required: true,
+    // },
+    // header_button_redirect_url: {
+    //   type: String,
+    //   required: true,
+    // },
+    // header_login_account_text: {
+    //   type: String,
+    //   required: true,
+    // },
+    // header_login_account_text_redirect_url: {
+    //   type: String,
+    //   required: true,
+    // },
+    // homepage_title: {
+    //   type: String,
+    //   required: true,
+    // },
+    login_header_text: {
       type: String,
       required: true,
     },
-    header_button_redirect_url: {
+login_header_redirect_url: {
       type: String,
       required: true,
     },
-    header_login_account_text: {
+header_section_welcome_message: {
       type: String,
       required: true,
     },
-    header_login_account_text_redirect_url: {
+header_section_main_heading: {
       type: String,
       required: true,
     },
-    homepage_title: {
+header_section_heading_subtitle: {
       type: String,
       required: true,
     },
+header_section_game_slider_url_1: {
+      type: String,
+      required: true,
+    },
+header_section_game_slider_url_2: {
+      type: String,
+      required: true,
+    },
+header_section_game_slider_url_3: {
+      type: String,
+      required: true,
+    },
+header_section_game_slider_url_4: {
+      type: String,
+      required: true,
+    },
+header_section_game_slider_url_5: {
+      type: String,
+      required: true,
+    },
+header_section_game_slider_url_6: {
+      type: String,
+      required: true,
+    },
+header_section_best_deal_caption: {
+      type: String,
+      required: true,
+    },
+header_section_best_deal_small_caption: {
+      type: String,
+      required: true,
+    },
+header_section_register_button: {
+      type: String,
+      required: true,
+    },
+header_section_register_button_url: {
+      type: String,
+      required: true,
+    },
+header_section_account_login: {
+      type: String,
+      required: true,
+    },
+header_section_account_login_url: {
+      type: String,
+      required: true,
+    }
   }
 };
 </script>
